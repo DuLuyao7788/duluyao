3fc186ef37af7cc5252df6686a7d4fdd7cb90d6d
DuLuyao7788 <1767999029@qq.com>
2020-02-18 18:24:20 +0800

    112

--- 2020-2-17/博客/服务器/server2.js
+++ 2020-2-17/博客/服务器/server2.js
@@ -4,6 +4,8 @@ const http = require('http')
 const url = require('url')
 // 引入 fs 模块
 const fs = require('fs')
+const path=require('http')
+
 
 http.createServer((req, res) => {
 
@@ -12,11 +14,19 @@ http.createServer((req, res) => {
     let urlPathName = myUrl.pathname
 
     let base = './html'
-
+    let extname=path.extname(pathname)
+    let mime="text/html;charset=utf-8"
     console.log(urlPathName)
+    if(extname==".png"){
+        mime="image/png"
+    }else if(extname==".js"){
+        mime="text/javascript"
+    }else if(extname==".css"){
+        mime="text/css"
+    }
 
     // 动态路由
-    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' })
+    res.writeHead(200, { 'Content-Type': mime })
     fs.readFile(base+urlPathName, (err, data) => {
         // 读取文件失败操作
         if (err) {
