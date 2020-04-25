725a0d6d42acf70741e948a948b1f33d171ba8a2
DuLuyao7788 <1767999029@qq.com>
2020-04-25 14:07:01 +0800

    周考

---
+++ 2020-4-25/zhou/app.js
@@ -0,0 +1,46 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    articles:[
+      { "id": "1", "title": "文章标题一", "content": "文章内容一" },
+      { "id": "2", "title": "文章标题二", "content": "文章内容二" },
+      { "id": "3", "title": "文章标题三", "content": "文章内容三" },
+      { "id": "4", "title": "文章标题四", "content": "文章内容四" },
+  ]
+  }
+  
+})
